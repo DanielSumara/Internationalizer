8cae6bd410bff0cbe687cf36ebe24ff51cd7d9bd
Daniel Sumara <daniel.sumara@gmail.com>
2017-03-13 08:49:26 +0100

    Add Repository class

---
+++ Models/Models/DataContexts/RepositoryContext.swift
@@ -0,0 +1,19 @@
+//
+//  RepositoryContext.swift
+//  Models
+//
+//  Created by Daniel Sumara on 11.03.2017.
+//  Copyright © 2017 Daniel Sumara. All rights reserved.
+//
+
+import Foundation
+
+public class RepositoryContext {
+    
+    // MARK:- Properties
+    
+    // MARK:- API
+    
+    
+    
+}
